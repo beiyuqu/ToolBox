386eb7a19e5458b22041ffc69773498b43f319d2
beiyuqu <326936595@qq.com>
2020-11-28 18:00:44 +0800

    update

--- VPS/install_sshkey.sh
+++ VPS/install_sshkey.sh
@@ -4,8 +4,13 @@
 # 自动配置SSH公钥登录，禁用密码登录
 # 时间: 2020年11月28日15点13分
 
+
+# ==================默认参数常量=======================
 USERNAME=beiyuqu
+PORT=13145
+
 PATH_CURR=`pwd`
+
 
 if apt --help >/dev/null 2>&1; then
     COMMOD='apt'
@@ -17,13 +22,10 @@ else
 fi
 
 # 依赖软件
+COMMOD update -y && COMMOD upgrade -y
 COMMOD install curl -y
 
 clear
-echo '======================================
-            SSH Key install
-            v1.0
-======================================='
 
 cd ~
 [ -d .ssh ] || mkdir .ssh 
@@ -41,10 +43,16 @@ sed -i "/PubkeyAuthentication no/c PubkeyAuthentication yes" sshd_config
 sed -i "/PasswordAuthentication yes/c PasswordAuthentication no" sshd_config
 sed -i "/RSAAuthentication yes/c RSAAuthentication yes" sshd_config
 sed -i "/PubkeyAuthentication yes/c PubkeyAuthentication yes" sshd_config
-service sshd restart
-service ssh restart
-systemctl restart sshd
-systemctl restart ssh
+# 端口修改
+sed -i "/Port 22/c Port $Port" sshd_config
+
+/etc/init.d/ssh restart
 
 cd $PATH_CURR
 rm -rf $0
+
+echo '=========================
+        禁用密码登录 yes
+        启用公钥登录 yes
+        修改端口$PORT yes
+=========================='
