371767bdabb94d52eceb8dc6ca3d6cb4e7c44d10
beiyuqu <326936595@qq.com>
2020-12-01 17:31:09 +0800

    Update install_sshkey.sh

--- VPS/install_sshkey.sh
+++ VPS/install_sshkey.sh
@@ -1,59 +1,33 @@
 #!/bin/bash
 
-# 用于新装系统
-# 自动配置SSH公钥登录，禁用密码登录
-# 时间: 2020年11月28日15点13分
+USERNAME="beiyuqu"
+[ $1 ] && USERNAME=$1
 
+SSH_FILT="/root/.ssh"
+SSH_KEYS="${SSH_FILT}/authorized_keys"
+SSH_CFG="/etc/ssh/sshd_config"
 
-# ==================默认参数常量=======================
-USERNAME=beiyuqu
-PORT=13145
+[ -d $SSH_FILT ] || mkdir $SSH_FILT
 
-PATH_CURR=`pwd`
-
-
-if apt --help >/dev/null 2>&1; then
-    COMMOD='apt-get'
-elif yum --help >/dev/null 2>&1; then
-    COMMOD='yum'
+if [ -e $SSH_KEYS ];then
+    read -p "$SSH_KEYS 公钥已经存在是否覆盖？(y/N)" yn
+    [[ ${yn,,} =~ ^y ]] && wget -q "https://github.com/${USERNAME}.keys" -O "$SSH_KEYS"
 else
-    echo '仅支持apt、yum安装软件'
-    exit 1
+    wget -q "https://github.com/${USERNAME}.keys" -O "$SSH_KEYS"
 fi
 
-# 依赖软件
-COMMOD update -y && COMMOD upgrade -y
-COMMOD install curl -y
+chmod 700 "${SSH_FILT}/authorized_keys"
+chmod 600 ${SSH_FILT}
 
-clear
-
-cd ~
-[ -d .ssh ] || mkdir .ssh 
-cd .ssh
-curl https://github.com/$USERNAME.keys > authorized_keys
-chmod 700 authorized_keys
-chmod 600 ../.ssh
-
-cd /etc/ssh/
-# 启用密码验证
-sed -i "/PasswordAuthentication no/c PasswordAuthentication no" sshd_config
-sed -i "/RSAAuthentication no/c RSAAuthentication yes" sshd_config
+# 启用密码登录
+sed -i "/^#*PasswordAuthentication/cPasswordAuthentication no" "$SSH_CFG"
+# 启用root账号密码登录
+sed -i "/^#*PermitRootLogin/cPermitRootLogin yes" "$SSH_CFG"
+# 允许rsa数字证书
+sed -i "/^#*RSAAuthentication/cRSAAuthentication yes" "$SSH_CFG"
 # 公钥验证
-sed -i "/PubkeyAuthentication no/c PubkeyAuthentication yes" sshd_config
-sed -i "/PasswordAuthentication yes/c PasswordAuthentication no" sshd_config
-sed -i "/RSAAuthentication yes/c RSAAuthentication yes" sshd_config
-sed -i "/PubkeyAuthentication yes/c PubkeyAuthentication yes" sshd_config
+sed -i "/^#*PubkeyAuthentication/cPubkeyAuthentication yes" "$SSH_CFG"
 
-service sshd restart
-service ssh restart
-systemctl restart sshd
-systemctl restart ssh
+/etc/init.d/ssh restart
 
-cd $PATH_CURR
-rm -rf $0
-
-echo '=========================
-        禁用密码登录 yes
-        启用公钥登录 yes
-        修改端口$PORT yes
-=========================='
+echo "设置完成！"
